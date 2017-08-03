ededf80f3514dd32b8b0cd8889ec14571be730ef
Jack Wright <jack.wright@nike.com>
2017-08-03 12:17:38 -0700

    Implemented register.. still no testing.

--- src/eureka_client.rs
+++ src/eureka_client.rs
@@ -4,7 +4,7 @@ use serde_json;
 use model::Instance;
 use errors::EurekaClientError;
 use hyper::{Client, Method, Request, Body, Uri, mime, Error as HyperError};
-use hyper::header::{AcceptEncoding, Encoding, UserAgent, ContentType, qitem};
+use hyper::header::{Accept, AcceptEncoding, Encoding, Headers, UserAgent, ContentType, ContentLength, qitem};
 use tokio_core::reactor::Handle;
 
 pub struct EurekaClient<'a> {
@@ -25,17 +25,32 @@ impl<'a> EurekaClient<'a> {
         }
     }
 
-    pub fn get_application_instances<'b>(&self, application_id: &str) -> impl Future<Item = Vec<Instance>, Error = EurekaClientError> {
+    pub fn register(&self, application_id: &str, instance: &Instance) -> impl Future<Item=(), Error=EurekaClientError> {
         let client = Client::new(self.handle);
-        let user_agent = "Rust Hyper/".to_string() + self.client_name.as_ref();
+        let path = "/eureka/v2/apps/".to_owned() + application_id;
+        let mut req: Request<Body> = Request::new(Method::Post, self.build_uri(path.as_ref()));
+        self.set_headers(req.headers_mut());
 
-        let uri: Uri = (self.eureka_cluster_url.to_owned() + "/eureka/v2/apps/" + application_id).parse().unwrap();
-        let mut req: Request<Body> = Request::new(Method::Post, uri);
-        req.headers_mut().set(AcceptEncoding(vec![qitem(Encoding::Gzip)]));
-        req.headers_mut().set(ContentType(mime::APPLICATION_JSON));
-        req.headers_mut().set(UserAgent::new(user_agent));
+        let json = serde_json::to_string(instance).unwrap();
+        req.headers_mut().set(ContentLength(json.len() as u64));
+        req.set_body(json);
 
         client.request(req).and_then(|res| {
+            debug!("get_application_instance: server response status: {}", res.status());
+            Ok(())
+        }).map_err(|e| {
+            EurekaClientError::ClientError(e)
+        })
+    }
+
+    pub fn get_application_instances<'b>(&self, application_id: &str) -> impl Future<Item=Vec<Instance>, Error=EurekaClientError> {
+        let client = Client::new(self.handle);
+        let path = "/eureka/v2/apps/".to_owned() + application_id;
+        let mut req: Request<Body> = Request::new(Method::Get, self.build_uri(path.as_ref()));
+        self.set_headers(req.headers_mut());
+
+        client.request(req).and_then(|res| {
+            debug!("get_application_instance: server response status: {}", res.status());
             res.body().concat2().and_then(move |body| {
                 serde_json::from_slice::<Vec<Instance>>(&body).map_err(|e| {
                     HyperError::Io(io::Error::new(io::ErrorKind::Other, e))
@@ -44,5 +59,17 @@ impl<'a> EurekaClient<'a> {
         }).map_err(|e| {
             EurekaClientError::ClientError(e)
         })
+    }
+
+    fn build_uri(&self, path: &str) -> Uri {
+        (self.eureka_cluster_url.to_owned() + path).parse().unwrap()
+    }
+
+    fn set_headers(&self, headers: &mut Headers) {
+        headers.set(AcceptEncoding(vec![qitem(Encoding::Gzip)]));
+        headers.set(Accept(vec![qitem(mime::APPLICATION_JSON)]));
+        headers.set(ContentType(mime::APPLICATION_JSON));
+        let user_agent = "Rust Hyper/".to_string() + self.client_name.as_ref();
+        headers.set(UserAgent::new(user_agent));
     }
 }
